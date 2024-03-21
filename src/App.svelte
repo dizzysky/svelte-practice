7b61ff8eb5f583adc5d5c9b5557a0d9055f6ce2b
Sanjid Dewan <dsanjid@gmail.com>
2024-03-21 18:25:31 -0400

    add event handler

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,22 @@
 <script>
     let name = "dude"; // Modified to use 'dude' as per your new snippet
     let src = "/image.gif"; // Path to your GIF image
+    let count = 0;
+
+    function increment() {
+        count += 1;
+    }
 </script>
 
 <h1>Hello {name.toUpperCase()}!</h1>
 <img {src} alt="{name} dances." />
 
 <p>{@html name}</p>
+
+<button>
+    Clicked {count}
+    {count === 1 ? "time" : "times"}
+</button>
 
 <style>
     /* Write your CSS here */
